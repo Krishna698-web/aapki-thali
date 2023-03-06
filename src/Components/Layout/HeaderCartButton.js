86a714689860c167b5ee3ce79a592e0e768aabb9
Krishan <kmkrider1999@gmail.com>
2023-03-06 15:40:35 +0530

    made responsive

--- src/Components/Layout/HeaderCartButton.js
+++ src/Components/Layout/HeaderCartButton.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState, useEffect } from 'react'
 import CartContext from '../../store/cart-context'
 import classes from './HeaderCartButton.module.css';
 
@@ -6,14 +6,35 @@ import {AiOutlineShoppingCart} from 'react-icons/ai'
 
 const HeaderCartButton = (props) => {
   const cartCtx = useContext(CartContext);
+  
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false); 
 
-  // const {items} = cartCtx;
-  const numOfCartItems = cartCtx.items.reduce((curNum, item) => {
+
+  const {items} = cartCtx;
+  const numOfCartItems = items.reduce((curNum, item) => {
     return curNum + item.amount;
   }, 0)
   
+  const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ''}`;
+
+  useEffect(() => {
+    if(items.length === 0){
+      return;
+    }
+    setBtnIsHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false)  
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    }
+  }, [items])
+  
+
   return (
-    <button onClick={props.onClick} className={classes.button}>
+    <button onClick={props.onClick} className={btnClasses}>
         <span className={classes.badge}>{numOfCartItems}</span>
         <AiOutlineShoppingCart className={classes.icon}/>
     </button>
